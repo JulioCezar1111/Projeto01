d6496dd2478e479ff192316871954a6a960e0e29
Julio Cezar Duarte Izidoro <105980233+JulioCezar1111@users.noreply.github.com>
2022-05-27 18:46:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MeuBlocoDeNotas
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Inicio());
+        }
+    }
+}
